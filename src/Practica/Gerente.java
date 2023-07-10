2a1dd90582dac2a39c8d2382a3999c508c69e16c
AngelMasterr <angelduarteprog@gmail.com>
2023-07-09 20:47:48 -0500

    practicando extends

--- src/Practica/Gerente.java
+++ src/Practica/Gerente.java
@@ -1,39 +1,8 @@
 package Practica;
 
-public class Gerente {
+// extends: toma todos los atributos de otra clase " Funcionario"
+public class Gerente extends Funcionario {
 	
-	private String nombre;
-	private String documento;
-	private double salario;
-	
-	// creando constructor
-	public Gerente() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	// creando getters and setters
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public String getDocumento() {
-		return documento;
-	}
-	public void setDocumento(String documento) {
-		this.documento = documento;
-	}
-	public double getSalario() {
-		return salario;
-	}
-	public void setSalario(double salario) {
-		this.salario = salario;
-	}
-	
-	// la bonificacion del gerente es un salario completo
-	public double getBonificación() {
-		return this.salario * 1;
-	}
+		
 	
 }
