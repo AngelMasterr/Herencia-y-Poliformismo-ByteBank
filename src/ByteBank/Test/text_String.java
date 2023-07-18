8b23ce77d24df10d883dd32bd35a51301baa2a48
AngelMasterr <angelduarteprog@gmail.com>
2023-07-17 19:15:17 -0500

    string: es un objeto, no un tipo de dato

---
+++ src/ByteBank/Test/text_String.java
@@ -0,0 +1,14 @@
+package ByteBank.Test;
+
+public class text_String {
+	
+	public static void main(String[] args) {
+		
+		// String: es un objeto, no es un tipo de dato como: int, double, byte, boolean etc.
+		String nombre = "alura";
+		String nombre2 = new String("alura");
+		
+	}
+
+}
+
