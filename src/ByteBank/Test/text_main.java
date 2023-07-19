3c08ce0543f1b28caa8ff3b712b1d792217f427e
AngelMasterr <angelduarteprog@gmail.com>
2023-07-18 21:09:03 -0500

    creando arrays

---
+++ src/ByteBank/Test/text_main.java
@@ -0,0 +1,28 @@
+package ByteBank.Test;
+
+public class text_main {
+	
+	public static void main(String[] args) {
+		
+		int edad1 = 20;
+		int edad2 = 15;
+		int edad3 = 67;
+		
+		// crear un array para agrupar varios datos
+		
+		int[] edades = new int[5]; // se creo un array con 5 posiciones todas iniciadas en cero "0"
+		edades[2] = 30; // en la tercera posicion del array tiene el valor de 30
+		edades[0] = 10;
+		edades[4] = 50;
+		
+		int tamaño_array = edades.length;
+		System.out.println(tamaño_array);  // longitud o tamaño del array
+		
+		for (int i = 0; i < tamaño_array; i++) {
+			System.out.println(edades[i]);
+		}
+			
+	}
+
+}
+
